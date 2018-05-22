82c047abcfa36e5cdd14842f4dd10c2f76d01909
A-to-Z-CSharp <abo.cooda@gmail.com>
2018-05-22 22:57:05 +0200

    بسم الله الرحمن الرحيم

---
+++ first program/first program/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // بسم الله الرحمن الرحيم
+            Console.WriteLine("\nwelcome to CSharp");
+            Console.ReadKey();
+        }
+    }
+}
