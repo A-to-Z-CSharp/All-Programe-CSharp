360f3dd8bd990bfdfc9c9f9eabf1822b13c5f4c7
A-to-Z-CSharp <abo.cooda@gmail.com>
2018-05-25 22:31:17 +0200

    literal characters

---
+++ literal_characters/literal_characters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace literal_characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // بسم الله الرحمن الرحيم
+            ulong myinvar = 1000l;
+
+            /*خاصة إدا كانت هناك علامة عشرية.float سواء كابتل أو سمول بدون مسافة بين قيمة المتغير من نوع f يجب وضع حرف 
+            /*خاصة إدا كانت هناك علامة عشرية.decimal سواء كابتل أو سمول بدون مسافة بين قيمة المتغير من نوع m يجب وضع حرف 
+            */
+
+            float myflvar = 10215.56F;
+            Double mydouvar = 5000.1;
+            decimal mydecvar = 66666666666.6m;
+
+            Console.WriteLine("\nmy int = {0} my double = {1} my decimal = {2}", myinvar,mydouvar,mydecvar);
+
+            //ويمكن طباعة القيم السابقة بطريقة مختصرة وجميلة وفرتها شركة مايكروسوفت تسمى دالة الاستقراء الداخلي
+
+            Console.WriteLine($"\n\nmy int = {myinvar} my double = {mydouvar} my decimal = {mydecvar}");
+            Console.ReadKey();
+         }
+    }
+}
